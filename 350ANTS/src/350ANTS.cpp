263510dbb5a0f71d85f10731a62198c4a663e515
JakeBrand <jabrand@ualberta.ca>
2012-10-22 14:23:32 -0600

    Init Commit

---
+++ 350ANTS/src/350ANTS.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 350ANTS.cpp
+// Author      : Eric Klinger & Jake Brand
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Ants World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
